a9746a8ba5eb8feb2e59e7a0a5d4a84a5d74a1f5
Varshinijana <varshinijanaa@gmail.com>
2024-06-15 18:22:13 +0530

    Add files via upload

---
+++ GUESS THE NUMBER USING PYTHON MINIPROJECT.py
@@ -0,0 +1,18 @@
+# guessing the number
+
+import random
+
+target=random.randint(1,100)
+
+while True:
+    userChoice=int(input("guess the target:"))
+    if(userChoice==target):
+        print("success:correct guess!!")
+        break
+    elif(userChoice<target):
+        print("your number was too small.take a bigger guess...")
+    else:
+        print("your number was too big.take a smaller guess...")
+
+
+print("------GAMEOVER------")
