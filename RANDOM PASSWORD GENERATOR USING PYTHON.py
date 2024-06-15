d0cbf0760e70f92170556692de595c79c5f97364
Varshinijana <varshinijanaa@gmail.com>
2024-06-15 18:54:05 +0530

    RANDOM PASSWORD GENERATOR USING PYTHON.py

--- RANDOM PASSWORD GENERATOR USING PYTHON.py
+++ RANDOM PASSWORD GENERATOR USING PYTHON.py
@@ -2,11 +2,11 @@
 import random
 import string
 
-pass_len=12
-charValues=string.ascii_letters+string.digits+string.punctuation
+pass_len = 12
+charValues = string.ascii_letters + string.digits + string.punctuation
 
-password=""
+password = ""
 for i in range(pass_len):
-    password+=random.choice(charValues)
+    password += random.choice(charValues)
 
 print("your random password is:",password)
