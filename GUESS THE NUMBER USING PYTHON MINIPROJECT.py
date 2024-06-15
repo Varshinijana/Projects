5099e5e4bc6ce47467a03e3e01b79deba8d756fb
Varshinijana <varshinijanaa@gmail.com>
2024-06-15 18:52:51 +0530

    Update GUESS THE NUMBER USING PYTHON.py

--- GUESS THE NUMBER USING PYTHON MINIPROJECT.py
+++ GUESS THE NUMBER USING PYTHON MINIPROJECT.py
@@ -2,14 +2,14 @@
 
 import random
 
-target=random.randint(1,100)
+target = random.randint(1,100)
 
 while True:
-    userChoice=int(input("guess the target:"))
-    if(userChoice==target):
+    userChoice = int(input("guess the target:"))
+    if(userChoice == target):
         print("success:correct guess!!")
         break
-    elif(userChoice<target):
+    elif(userChoice < target):
         print("your number was too small.take a bigger guess...")
     else:
         print("your number was too big.take a smaller guess...")
